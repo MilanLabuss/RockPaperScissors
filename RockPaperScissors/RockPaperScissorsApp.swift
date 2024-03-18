12e84a8cda565f872f87fcbc48d6a8073638dbbc
MilanLabuss <milanlabus29@gmail.com>
2024-03-18 14:06:32 +0000

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Milan Labus on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
